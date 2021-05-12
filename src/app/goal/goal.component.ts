48e43c2c5324241df14c3435b1bd7eba46bb7845
[jimmy-oss] <jimmynjonge51@gmail.com>
2021-05-12 14:34:09 +0300

    my codes

---
+++ src/app/goal/goal.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Goal } from '../goal';
+
+@Component({
+  selector: 'app-goal',
+  templateUrl: './goal.component.html',
+  styleUrls: ['./goal.component.css']
+})
+export class GoalComponent implements OnInit {
+
+  goals: Goal[] = [
+    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
+    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot'),
+    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon'),
+    new Goal(4,'Get Dog Food','Pupper likes expensive snacks'),
+    new Goal(5,'Solve math homework','Damn Math'),
+    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord'),
+  ];
+  toggleDetails(index:any){
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
