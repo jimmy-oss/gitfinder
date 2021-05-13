15b8177adf622fad071e85f0903ffc9a5577d0cb
[jimmy-oss] <jimmynjonge51@gmail.com>
2021-05-13 13:50:09 +0300

    emitting files

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -13,7 +13,7 @@ export class GoalComponent implements OnInit {
     new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
     new Goal(2,'Buy Cookies','I have to buy cookies for the parrot'),
     new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon'),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks'),
+    new Goal(4,'Get Dog Food','Puppet likes expensive snacks'),
     new Goal(5,'Solve math homework','Damn Math'),
     new Goal(6,'Plot my world domination plan','Cause I am an evil overlord'),
   ];
