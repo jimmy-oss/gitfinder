2989ec8d09a8be74a9a1e2f3790d1ce130f14921
[jimmy-oss] <jimmynjonge51@gmail.com>
2021-05-15 00:52:10 +0300

    quote files

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -9,12 +9,12 @@ import { Goal } from '../goal';
 export class GoalComponent implements OnInit {
 
   goals: Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
-    new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
+    new Goal(1, 'Dwell on the beauty of life', 'Watch the stars, and see yourself running with them.Marcus Aurelius',new Date(2021,3,14)),
+    new Goal(2,'Nothing is impossible', 'The word itself says ‘I’m possible’!” Audrey Hepburn',new Date(2021,6,9)),
+    new Goal(3,'Courage doesn’t always roar', 'Sometimes courage is the little voice at the end of the day that says ‘I’ll try again tomorrow. Mary Anne Radmacher',new Date(2021,1,12)),
+    new Goal(4,'Count your age by friends', 'Not years. Count your life by smiles, not tears.”John Lennon',new Date(2021,0,18)),
+    new Goal(5,'Laughter is timeless', 'Imagination has no age. And dreams are forever.” Walt Disney',new Date(2021,2,14)),
+    new Goal(6,'There is only one thing that makes a dream impossible to achieve', 'The fear of failure.”Paulo Coelho',new Date(2021,3,14)),
   ];
   completeGoal(isComplete:boolean, index: any){
     if (isComplete) {
