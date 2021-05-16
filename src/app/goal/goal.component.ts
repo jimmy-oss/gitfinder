96084f138955fff63dee338159e300d347d897c3
[jimmy-oss] <jimmynjonge51@gmail.com>
2021-05-16 20:32:40 +0300

    changing date

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -12,12 +12,12 @@ export class GoalComponent implements OnInit {
     
 
   goals: Goal[] = [
-    new Goal(1, 'Dwell on the beauty of life', 'Watch the stars, and see yourself running with them.Marcus Aurelius',new Date(2021,3,14)),
+    new Goal(1, 'Dwell on the beauty of life', 'Watch the stars, and see yourself running with them.Marcus Aurelius',new Date(2021,6,7)),
     new Goal(2,'Nothing is impossible', 'The word itself says ‘I’m possible’!” Audrey Hepburn',new Date(2021,6,9)),
     new Goal(3,'Courage doesn’t always roar', 'Sometimes courage is the little voice at the end of the day that says ‘I’ll try again tomorrow. Mary Anne Radmacher',new Date(2021,1,12)),
-    new Goal(4,'Count your age by friends', 'Not years. Count your life by smiles, not tears.”John Lennon',new Date(2021,0,18)),
-    new Goal(5,'Laughter is timeless', 'Imagination has no age. And dreams are forever.” Walt Disney',new Date(2021,2,14)),
-    new Goal(6,'There is only one thing that makes a dream impossible to achieve', 'The fear of failure.”Paulo Coelho',new Date(2021,3,14)),
+    new Goal(4,'Count your age by friends', 'Not years. Count your life by smiles, not tears.”John Lennon',new Date(2021,5,10)),
+    new Goal(5,'Laughter is timeless', 'Imagination has no age. And dreams are forever.” Walt Disney',new Date(2021,5,8)),
+    new Goal(6,'There is only one thing that makes a dream impossible to achieve', 'The fear of failure.”Paulo Coelho',new Date(2021,7,9)),
   ];
   completeGoal(isComplete:boolean, index: any){
     if (isComplete) {
